f4b6608586ae98a06e3ab5e3cc5dc5008ff4da1e
apod-1 <andrew.n.podosenov@gmail.com>
2024-07-23 20:00:31 -0500

    Update Strategy_Play.py

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -514,7 +514,7 @@ def run_streamlit_app(validate_df, start_date, end_date):
         }
         .top-frame video {
             position: absolute;
-            top: -20%;
+            top: -15%;
             bottom: -30%;
             left: 0;
             width: 100%;
