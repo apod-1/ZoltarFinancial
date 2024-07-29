13fa56e112294f0c246800a1a011014a918ed136
apod-1 <andrew.n.podosenov@gmail.com>
2024-07-29 11:01:17 -0500

    cosmetic

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -1003,7 +1003,7 @@ def run_streamlit_app(validate_df, start_date, end_date):
 
     st.title("Interactive Trading Strategy Evaluation and Recommendation Engine")
     st.write("IMPORTANT: For best experience please use on high-memory device (optimization under way to address lackluster mobile experience). Thank you for your patience!")
-    st.write("Date range:", combined_validate_df['Week'].min(), "to", combined_validate_df['Week'].max())
+    st.write("Date range:", combined_validate_df['Week'].min().strftime('%m-%d-%Y'), "to", combined_validate_df['Week'].max().strftime('%m-%d-%Y'))
     st.write("Number of unique symbols:", combined_validate_df['Symbol'].nunique())
 
     # Instructions section
