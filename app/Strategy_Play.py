e64b313922a790c9f247ee0a896dc25cb427250c
apod-1 <andrew.n.podosenov@gmail.com>
2024-07-29 00:51:21 -0500

    Update Strategy_Play.py

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -1424,36 +1424,36 @@ def run_streamlit_app(validate_df, start_date, end_date):
         else:
             st.sidebar.error("Please enter a valid email address.")
 
-    # st.markdown(
-    #     """
-    #     <style>
-    #     .button-container {
-    #         position: fixed;
-    #         bottom: 20px;
-    #         right: 20px;
-    #         z-index: 9999;
-    #     }
-    #     .stButton button {
-    #         font-size: 10px;
-    #         color: blue;
-    #         background: none;
-    #         border: none;
-    #         padding: 10px;
-    #         cursor: pointer;
-    #     }
-    #     </style>
-    #     <div class="button-container">
-    #         <button id="show-image-button" class="stButton">π</button>
-    #     </div>
-    #     <script>
-    #     const showImageButton = document.getElementById("show-image-button");
-    #     showImageButton.onclick = function() {
-    #         window.parent.postMessage({type: 'streamlit:setComponentValue', value: true}, '*');
-    #     };
-    #     </script>
-    #     """,
-    #     unsafe_allow_html=True
-    # )
+    st.markdown(
+        """
+        <style>
+        .button-container {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            z-index: 9999;
+        }
+        .stButton button {
+            font-size: 50px;
+            color: blue;
+            background: none;
+            border: none;
+            padding: 10px;
+            cursor: pointer;
+        }
+        </style>
+        <div class="button-container">
+            <button id="show-image-button" class="stButton">π</button>
+        </div>
+        <script>
+        const showImageButton = document.getElementById("show-image-button");
+        showImageButton.onclick = function() {
+            window.parent.postMessage({type: 'streamlit:setComponentValue', value: true}, '*');
+        };
+        </script>
+        """,
+        unsafe_allow_html=True
+    )
 
 
 
@@ -1478,7 +1478,7 @@ def run_streamlit_app(validate_df, start_date, end_date):
     if 'pi_clicked' not in st.session_state:
         st.session_state.pi_clicked = False
     # Use a container to hold the button that will be hidden
-    # button_container = st.empty()
+    button_container = st.empty()
     
     # Interactive menu section on the right pane
     menu_options = ["About", "Methodology", "Services", "ZF Blockchain", "Investors"]
@@ -1551,7 +1551,7 @@ def run_streamlit_app(validate_df, start_date, end_date):
     
     if st.session_state.show_image:
         # Title of the Section
-        st.subheader("******************     Stock Recommendations    *******************")
+        st.subheader("Stock Recommendations")
 
         # Row 1: Recommendations
         col1, col2, col3 = st.columns(3)
