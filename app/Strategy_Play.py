fed714582fc2f094cb82befc9c170ce9e71c06e0
apod-1 <andrew.n.podosenov@gmail.com>
2024-08-02 17:39:47 -0500

    +1

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -2058,13 +2058,14 @@ def run_streamlit_app(validate_df, start_date, end_date):
     
     # Assuming `combined_df` has a 'Week' column
     max_week = combined_validate_df['Week'].max().strftime('%m-%d-%Y')
-    
+    next_bd = (max_week + BDay(1)).strftime('%m-%d-%Y')
+    next_bd_comb = combined_validate_df['Week'].max().strftime('%m-%d-%Y')
 
 
     # Display image when button is clicked
     if st.session_state.show_image:
         # Title of the Section
-        st.markdown(f"<h2 style='text-align: center;'>Recommendations for {(max_week + BDay(1)).strftime('%Y-%m-%d')}</h2>", unsafe_allow_html=True)
+        st.markdown(f"<h2 style='text-align: center;'>Recommendations for {next_bd}</h2>", unsafe_allow_html=True)
     
         # Generate rankings_df for the last 3 days
         end_date = combined_validate_df['Week'].max()
@@ -2166,8 +2167,8 @@ def run_streamlit_app(validate_df, start_date, end_date):
             else:
                 st.write("Large Cap Performance image not found")
     
-        # New Section: Overall Zoltar Stock Picks{(max_week + BDay(1)).strftime('%Y-%m-%d')}
-        st.markdown(f"<h2 style='text-align: center;'>Overall Zoltar Stock Picks - {(combined_validate_df['Week'].max() + BDay(1)).strftime('%m-%d-%Y')}</h2>", unsafe_allow_html=True)
+        # New Section: Overall Zoltar Stock Picks
+        st.markdown(f"<h2 style='text-align: center;'>Overall Zoltar Stock Picks - {next_bd_comb}</h2>", unsafe_allow_html=True)
     
         # Display images in a single column
         all_rec_1 = get_latest_file("expected_returns_path_ALL_")
