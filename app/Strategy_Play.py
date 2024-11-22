d4070b7e86eee2f90782bb4968620eeded4f47aa
apod-1 <andrew.n.podosenov@gmail.com>
2024-11-22 00:05:13 -0600

    local environment vars hide

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -95,13 +95,13 @@ pre_prompt_low = ""
 np.random.seed(42)
 
 # Load environment variables
-sys.path.append('C:/Users/apod7/StockPicker/scripts')
-load_dotenv()
-RH_Login = os.getenv('RH_Login')
-RH_Pass = os.getenv('RH_Pass')
-GMAIL_ACCT = os.getenv('GMAIL_ACCT')
-GMAIL_PASS = os.getenv('GMAIL_PASS')
-OPENAI_API = os.getenv('API_KEY')
+# sys.path.append('C:/Users/apod7/StockPicker/scripts')
+# load_dotenv()
+# RH_Login = os.getenv('RH_Login')
+# RH_Pass = os.getenv('RH_Pass')
+# GMAIL_ACCT = os.getenv('GMAIL_ACCT')
+# GMAIL_PASS = os.getenv('GMAIL_PASS')
+# OPENAI_API = os.getenv('API_KEY')
 
 # Initialize session state
 if 'show_confirmation' not in st.session_state:
