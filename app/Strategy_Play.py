08ba10a684e8f506a6159c5f6cccb7b39ccbd6a5
apod-1 <andrew.n.podosenov@gmail.com>
2024-11-25 01:44:52 -0600

    update ranking columns

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -8845,9 +8845,25 @@ def run_streamlit_app(high_risk_df, low_risk_df, full_start_date, full_end_date)
     filtered_versions = filtered_versions[:10]
     # Get the data for selected versions with filters applied
     high_risk_df_long, low_risk_df_long = select_versions2(10, None, default_time_slots)
+
+    # Get all date columns
+    date_columns = [col for col in low_risk_df.columns if isinstance(col, pd.Timestamp)]
+    
+    # Filter date columns based on the selected date range
+    date_columns = [col for col in date_columns if start_date <= col <= end_date]
+    
+    # if not date_columns:
+    #     st.error(f"No data available for the selected date range for {ranking_type} rankings.")
+    #     return
+    
+    # Use the latest date column in the selected range for ranking
+    latest_date = max(date_columns)
+    ranking_column = latest_date
+    
     # Sort the filtered DataFrame
-    sorted_df_low = low_risk_df.sort_values(by="Low_Risk_Score", ascending=False).reset_index(drop=True)
-    sorted_df_high = high_risk_df.sort_values(by="High_Risk_Score", ascending=False).reset_index(drop=True)
+    sorted_df_low = low_risk_df.sort_values(by=ranking_column, ascending=False).reset_index(drop=True)
+    sorted_df_high = high_risk_df.sort_values(by=ranking_column, ascending=False).reset_index(drop=True)
+
     # Sort both DataFrames by 'Symbol', 'Version', and 'Date' in descending order
     high_risk_df_long = high_risk_df_long.sort_values(by=['Symbol', 'Version', 'Date'], ascending=[True, True, False])
     low_risk_df_long = low_risk_df_long.sort_values(by=['Symbol', 'Version', 'Date'], ascending=[True, True, False])
@@ -8911,6 +8927,7 @@ def run_streamlit_app(high_risk_df, low_risk_df, full_start_date, full_end_date)
 
     merged_df_low = pd.merge(low_risk_df, combined_fundamentals_df, on='Symbol', how='left')
     merged_df_high = pd.merge(high_risk_df, combined_fundamentals_df, on='Symbol', how='left')
+
     
     # Filter for custom stocks and get the latest date for each stock
     custom_df_low = merged_df_low[merged_df_low['Symbol'].isin(default_stocks_low)]
