33610237512ceda7c94de7a89653b1d9327711ed
apod-1 <andrew.n.podosenov@gmail.com>
2024-11-27 19:14:01 -0600

    10

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -10205,13 +10205,21 @@ def run_streamlit_app(high_risk_df, low_risk_df, full_start_date, full_end_date)
     # centered_header_main("***  Please ask Zoltar your question below, or scroll to the top to proceed. ***")
     centered_header_main("""Ask Zoltar your question below to receive insights and guidance tailored to your needs. """)
     st.write()
-    st.write("Alternatively, feel free to **scroll to the top** to explore other features of the app")
+    # st.write("Alternatively, feel free to **scroll to the top** to explore other features of the app")
 
-    st.write()
+    # st.write()
    
-    st.write("Let's get started on your journey to smarter investing!")
-    st.write()
-    st.write("May the riches be with you...")
+    # st.write("Let's get started on your journey to smarter investing!")
+    # st.write()
+    # st.write("May the riches be with you...")
+
+    # Center the text using Markdown
+    st.markdown("<h3 style='text-align: center;'>Alternatively, feel free to <strong>scroll to the top</strong> to explore other features of the app</h3>", unsafe_allow_html=True)
+    
+    st.markdown("<h3 style='text-align: center;'>Let's get started on your journey to smarter investing!</h3>", unsafe_allow_html=True)
+    
+    st.markdown("<h3 style='text-align: center;'>May the riches be with you...</h3>", unsafe_allow_html=True)
+
         
     # # Register the callback function
     # query_params = st.query_params()
