d68eb45379eef6ac822998fcb9846d7da5b852ff
apod-1 <andrew.n.podosenov@gmail.com>
2025-01-03 14:12:08 -0600

    Automated commit: Fri 01/03/2025 14:12:08.05

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -3116,30 +3116,30 @@ def display_interactive_rankings(rankings_df, ranking_type, fundamentals_df, fil
             
 
 # 1.2.25 - sticking it here now...
-        # # 11.12.24 - section to scroll to the top
-        # # Add custom HTML and CSS for the "Go to Top" button
-        # # Add custom HTML and CSS for the "Go to Top" button
-        st.markdown("""
-            <style>
-                .go-to-top {
-                    position: fixed;
-                    bottom: 20px;
-                    right: 100px;
-                    width: 40px;
-                    height: 40px;
-                    background-color: #4CAF50; /* Green background */
-                    color: white; /* White text */
-                    border: none; /* No border */
-                    border-radius: 5px; /* Rounded corners */
-                    cursor: pointer; /* Pointer cursor on hover */
-                    font-size: 24px; /* Larger text for the arrow */
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-            </style>
-            <button class="go-to-top" onclick="window.scrollTo(0, 0);">↑</button>
-        """, unsafe_allow_html=True)
+    # # 11.12.24 - section to scroll to the top
+    # # Add custom HTML and CSS for the "Go to Top" button
+    # # Add custom HTML and CSS for the "Go to Top" button
+    st.markdown("""
+        <style>
+            .go-to-top {
+                position: fixed;
+                bottom: 20px;
+                right: 150px;
+                width: 40px;
+                height: 40px;
+                background-color: #4CAF50; /* Green background */
+                color: white; /* White text */
+                border: none; /* No border */
+                border-radius: 5px; /* Rounded corners */
+                cursor: pointer; /* Pointer cursor on hover */
+                font-size: 24px; /* Larger text for the arrow */
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+        </style>
+        <button class="go-to-top" onclick="window.scrollTo(0, 0);">↑</button>
+    """, unsafe_allow_html=True)
 
 
 # 10.14.24 - expanded version 2
