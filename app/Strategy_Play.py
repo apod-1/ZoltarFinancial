9a024bca59cbe3b0ff7bba380bdba05c7a326380
apod-1 <andrew.n.podosenov@gmail.com>
2025-01-04 00:09:02 -0600

    no duplication

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -9855,7 +9855,11 @@ def run_streamlit_app(high_risk_df, low_risk_df, full_start_date, full_end_date)
                     
                     if update_type == 'Daily':
                         # For Daily updates, keep only the last record for each Symbol and Date
-                        df = df.groupby(['Symbol', df['Date'].dt.date]).last().reset_index()
+                        df = df.groupby(['Symbol', df['Date'].dt.date]).agg({
+                            'Score': 'last',
+                            'Close_Price': 'last'
+                        }).reset_index()
+                        
                         # Ensure Date is in datetime format after grouping
                         df['Date'] = pd.to_datetime(df['Date'])
                     else:
@@ -9868,8 +9872,7 @@ def run_streamlit_app(high_risk_df, low_risk_df, full_start_date, full_end_date)
                     return df, new_start_date, new_end_date
                 
                 # Usage in generate_daily_rankings_strategies():
-                selected_df, start_date, end_date = prepare_longitudinal_data(high_risk_df_long, low_risk_df_long, risk_level, start_date, end_date, update_type)
-                # Print the results
+                selected_df, start_date, end_date = prepare_longitudinal_data(high_risk_df_long, low_risk_df_long, risk_level, start_date, end_date, update_type)                # Print the results
                 print(selected_df.columns)
                 print(selected_df.head(5))    
                 # print(selected_df[selected_df['Symbol'] == 'SPY'].columns)                
