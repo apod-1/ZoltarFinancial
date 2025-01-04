54b0c87c23510f5ba5f1ddfe38d45c2efcc596f1
apod-1 <andrew.n.podosenov@gmail.com>
2025-01-03 19:38:05 -0600

    input datasets

--- app/Strategy_Play.py
+++ app/Strategy_Play.py
@@ -9795,7 +9795,7 @@ def run_streamlit_app(high_risk_df, low_risk_df, full_start_date, full_end_date)
                     return pivoted_df, new_start_date, new_end_date
                 
                 # Usage in generate_daily_rankings_strategies():
-                selected_df, start_date, end_date = prepare_longitudinal_data(high_risk_df, low_risk_df, risk_level, start_date, end_date)
+                selected_df, start_date, end_date = prepare_longitudinal_data(high_risk_df_long, low_risk_df_long, risk_level, start_date, end_date)
 
                 print(selected_df[selected_df['Symbol'] == 'SPY'].columns)                
                 # # Usage in generate_daily_rankings_strategies():
