75c7a44d1908b127cc3bcc4be3040044ad6d1a27
apod-1 <andrew.n.podosenov@gmail.com>
2025-01-16 11:57:22 -0600

    activate 3.11 in run_apps.sh

--- run_apps.sh
+++ run_apps.sh
@@ -6,7 +6,7 @@ PYTHON_VERSION=$(python --version)
 if [[ "$PYTHON_VERSION" == *"3.12"* ]]; then
     echo "Switching to Python 3.11 for compatibility."
     # Activate a virtual environment with Python 3.11 if necessary
-    # source /path/to/python3.11/venv/bin/activate
+    source /path/to/python3.11/venv/bin/activate
 fi
 
 streamlit run app/Strategy_Play.py --server.port 8501 &
