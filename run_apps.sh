17d0c97e9a8244df246130151ca0ebe76c6a96be
apod-1 <andrew.n.podosenov@gmail.com>
2025-01-16 17:49:23 -0600

    run_apps.sh updated to --server.enableCORS false --server.enableXsrfProtection false &

--- run_apps.sh
+++ run_apps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Activate the appropriate virtual environment if necessary
-#source /path/to/your/venv/bin/activate
+# source /path/to/your/venv/bin/activate
 
 # Start both Streamlit apps
-streamlit run app/Strategy_Play.py --server.port 8501 &
-streamlit run CustomizeMyCV/app/CustomizeMyCV.py --server.port 8502 &
+streamlit run app/Strategy_Play.py --server.port 8501 --server.enableCORS false --server.enableXsrfProtection false &
+streamlit run CustomizeMyCV/app/CustomizeMyCV.py --server.port 8502 --server.enableCORS false --server.enableXsrfProtection false &
 
 wait
