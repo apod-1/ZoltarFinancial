1920de296d8267ee236acb71357bbd8eb4c92ba6
apod-1 <andrew.n.podosenov@gmail.com>
2025-01-16 10:45:52 -0600

    chage run_apps.sh line

--- run_apps.sh
+++ run_apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 streamlit run app/Strategy_Play.py --server.port 8501 &
-streamlit run app/CustomizeMyCV.py --server.port 8502 &
+streamlit run CustomizeMyCV/app/CustomizeMyCV.py --server.port 8502 &
 
 wait
