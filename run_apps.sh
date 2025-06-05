2b0b5f765dfbf6aec2a56988d77aa27c6b0310f7
apod-1 <andrew.n.podosenov@gmail.com>
2025-06-05 11:32:51 -0500

    Automated commit: Thu 06/05/2025 11:32:51.20

--- run_apps.sh
+++ run_apps.sh
@@ -6,5 +6,6 @@
 # Start both Streamlit apps
 streamlit run app/Strategy_Play.py --server.port 8501 --server.enableCORS false --server.enableXsrfProtection false &
 streamlit run CustomizeMyCV/app/CustomizeMyCV.py --server.port 8502 --server.enableCORS false --server.enableXsrfProtection false &
+streamlit run ZoltarResearch/zoltar_stock_research_agent.py --server.port 8503 --server.enableCORS false --server.enableXsrfProtection false &
 
 wait
